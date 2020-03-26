fa5f632a7504672a1214fdc9214179d767766040
joaovitorblabres <joaovitorlabres@gmail.com>
2020-03-26 16:10:34 -0300

    new changes

--- checkBib.py
+++ checkBib.py
@@ -1,10 +1,10 @@
-import bibtexparser, sys, re, codecs
+import bibtexparser, sys, re, codecs, unicodedata
 from bibtexparser.customization import homogenize_latex_encoding
 warning = 0
 
 def erroID(i, ID, default):
     print("Padrão incorreto no campo ID da referencia:", i, "(", ID,
-            ");\nVerifique o(s) nome(s) do(s) autor(es), um possível padrão é colocar o sobrenome capitalizado e depois o ano:")
+            ");\nVerifique o(s) nome(s) do(s) autor(es), um possível padrão é:")
     print(default)
     print("---------------\\/---------------")
 
@@ -17,14 +17,14 @@ def with1author(authors, default, i, ID):
     author = authors[0].split(',')
     if(len(author) == 1):
         author = authors[0].split(' ')
-        default = author[-1].capitalize()+year
+        default = author[-1]+year
         if not ID.startswith(default):
             callWarn(i, ID, default)
             return 0
 
     else:
         author = author[0].split(' ')
-        default = author[-1].capitalize()+year
+        default = author[-1]+year
         if not ID.startswith(default):
             callWarn(i, ID, default)
             return 0
@@ -35,15 +35,20 @@ def with2authors(authors, default, i, ID):
     author1 = authors[0].split(',')
     author2 = authors[1].split(',')
     if(len(author1) == 1 or len(author2) == 1):
+        if len(author1) > 1:
+            authors[0] = author1[0]
+        if len(author2) > 1:
+            authors[1] = author2[0]
         author1 = authors[0].split(' ')
         author2 = authors[1].split(' ')
-        default = author1[-1].capitalize()+'&'+author2[-1].capitalize()+year
+        default = author1[-1]+'&'+author2[-1]+year
         if not ID.startswith(default):
             callWarn(i, ID, default)
             return 0
-
     else:
-        default = author1[0].capitalize()+'&'+author2[0].capitalize()+year
+        author1 = author1[0].split(' ')
+        author2 = author2[0].split(' ')
+        default = author1[-1]+'&'+author2[-1]+year
         if not ID.startswith(default):
             callWarn(i, ID, default)
             return 0
@@ -54,7 +59,7 @@ def withmanyauthors(authors, default, i, ID):
     author = authors[0].split(',')
     if(len(author) == 1):
         author = authors[0].split(' ')
-        default = author[-1].capitalize()+'+'+year
+        default = author[-1]+'+'+year
 
         if not ID.startswith(default):
             callWarn(i, ID, default)
@@ -62,7 +67,7 @@ def withmanyauthors(authors, default, i, ID):
 
     else:
         author = author[0].split(' ')
-        default = author[-1].capitalize()+'+'+year
+        default = author[-1]+'+'+year
         if not ID.startswith(default):
             callWarn(i, ID, default)
             return 0
@@ -78,7 +83,7 @@ try:
     with open(sys.argv[1]) as bibtex_file:
         bib_database = bibtexparser.bparser.BibTexParser(common_strings=True, ignore_nonstandard_types=False, interpolate_strings=False).parse_file(bibtex_file)
 except UnicodeDecodeError as e:
-    print("invalid utf-8 at line", str(i+1))
+    print("invalid utf-8 near of line", str(i+1))
     print(e)
     exit()
 
@@ -91,11 +96,17 @@ try:
         unCheck = ['booklet', 'manual', 'proceedings']
 
         if bib_database.entries[i]['ENTRYTYPE'] not in unCheck:
-            checks = bib_database.entries[i]['author']
-            for rep in ['\\v', '\\c', '\\', '\'', '\"', '{', '}', '~', '^']:
+            if 'author' not in bib_database.entries[i]:
+                checks = bib_database.entries[i]['editor']
+            else:
+                checks = bib_database.entries[i]['author']
+            for rep in ['\\v', '\\c ', '\\c', '\\`', '\\', '\'', '\"', '{', '}', '~', '^']:
                 checks = checks.replace(rep, '')
             checks = checks.replace('\n', ' ')
+            if '-' in checks and '-' not in ID:
+                checks = checks.replace('-', '')
 
+            checks = unicodedata.normalize('NFD', checks).encode('ascii', 'ignore').decode("utf-8")
             authors = checks.split(' and ')
             year = bib_database.entries[i]['year']
 
